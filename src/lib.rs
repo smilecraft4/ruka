80fa8779381058643fc4a294f33a24a446c33a98
smilecraft4 <69019569+smilecraft4@users.noreply.github.com>
2023-08-19 18:52:05 +0200

    A fresh start

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+//! TODO: documentation on how to use
+
+pub mod error;
+pub mod prelude;
--- src/prelude.rs
+++ src/prelude.rs
@@ -2,8 +2,6 @@
 
 pub use crate::error::Error;
 
-/// Ruka's error result
 pub type Result<T> = core::result::Result<T, Error>;
 
-// Generic wrapper for newtype pattern
 pub struct W<T>(pub T);
